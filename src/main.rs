3513fdc9258e4c08d18868b4273a3833bfbd30fd
Artem Borosovskiy <bytefu@gmail.com>
2015-11-12 11:49:58 +0200

    Fix line endings

--- src/main.rs
+++ src/main.rs
@@ -24,21 +24,21 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
-    
+
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
     };
-    
+
     if matches.opt_present("h") {
         print_usage(&args[0], opts);
         return;
     }
-    
+
     if matches.free.is_empty() {
         print_usage(&args[0], opts);
     }
-    
+
     for file in matches.free {
         process(Path::new(&file));
     }
@@ -46,7 +46,7 @@ fn main() {
 
 fn process(path: &Path) {
     println!(">> Processing {}...", path.to_string_lossy());
-    
+
     let mut file = match File::open(&path) {
         Ok(f) => f,
         Err(err) => {
@@ -54,7 +54,7 @@ fn process(path: &Path) {
             return
         }
     };
-    
+
     let mut content = String::new();
     match file.read_to_string(&mut content) {
         Ok(_) => {},
@@ -63,7 +63,7 @@ fn process(path: &Path) {
             return
         }
     };
-    
+
     let path_str_owned = path.to_string_lossy();
     let path_str = path_str_owned.as_ref();
     let mut lexer = match Lexer::new(path_str, &content) {
@@ -73,7 +73,7 @@ fn process(path: &Path) {
     		return;
     	}
     };
-    
+
     println!("{}", lexer);
 //    let mut token_count = 0;
 //    let mut last_line_index = 0;
@@ -82,9 +82,9 @@ fn process(path: &Path) {
 //        last_line_index = token.location.line_index;
 ////        println!("{}", token);
 //    }
-//    
+//
 //    println!(">> {} tokens, {} lines", token_count, last_line_index + 1);
-    
+
     let mut parser = Parser::new(lexer);
     let ast = parser.parse();
     match ast {
