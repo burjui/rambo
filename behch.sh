1ea6c30dae28e4ae7c108c382311c83573cca98b
Artem Borisovskiy <bytefu@gmail.com>
2018-12-30 14:39:24 +0200

    Allow benchmarking both release and debug builds

--- behch.sh
+++ behch.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
-for i in x.rambo y.rambo z.rambo str.rambo; do cargo build --release --quiet > /dev/null && /usr/bin/time -v ./target/release/rambo test-data/$i -w 2>&1 | grep Elapsed | perl -pe 's/.*: (.*)/\1/g'; done
+set -e
+
+case "$1" in
+	release)
+		MODE="--release"
+		TARGET="release"
+		;;
+	debug)
+		MODE=""
+		TARGET="debug"
+		;;
+	*)
+		echo "Usage: $0 <release|debug>"
+		exit 1
+		;;
+esac
+cargo build $MODE
+for i in x.rambo y.rambo z.rambo str.rambo; do /usr/bin/time -v ./target/$TARGET/rambo test-data/$i -w 2>&1 | grep Elapsed | perl -pe 's/.*: (.*)/\1/g'; done
