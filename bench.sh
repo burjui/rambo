3e2dfbcf425602f2f2226b6c97dd954da389a0e7
Artem Borisovskiy <bytefu@gmail.com>
2019-02-26 02:31:32 +0200

    Pass bench.sh arguments to the compiler

--- bench.sh
+++ bench.sh
@@ -15,8 +15,9 @@ case "$1" in
 		exit 1
 		;;
 esac
+shift
 cargo build $MODE
 for i in x.rambo y.rambo z.rambo str.rambo; do
 	echo -n "$i: "
-	/usr/bin/time -f "%E" ./target/$TARGET/rambo test-data/$i -w > /dev/null | perl -pe 's/.\w+: (.*)/\1/g'
+	/usr/bin/time -f "%E" ./target/$TARGET/rambo test-data/$i $@ > /dev/null | perl -pe 's/.\w+: (.*)/\1/g'
 done
