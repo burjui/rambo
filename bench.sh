e91f649694e44e5a07d429aaf149cb753fb35ea5
Artem Borisovskiy <bytefu@gmail.com>
2019-03-02 23:08:05 +0200

    Add a parameter to bench.sh for source directory

--- bench.sh
+++ bench.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-case "$1" in
+SRC_DIR="$1"
+
+case "$2" in
 	release)
 		MODE="--release"
 		TARGET="release"
@@ -11,13 +13,16 @@ case "$1" in
 		TARGET="debug"
 		;;
 	*)
-		echo "Usage: $0 <release|debug>"
+		echo "Usage: $0 <SOURCE DIR> <release|debug> [OPTIONS]..."
 		exit 1
 		;;
 esac
+
 shift
+shift
+
 cargo build $MODE
 for i in x.rambo y.rambo z.rambo str.rambo; do
 	echo -n "$i: "
-	/usr/bin/time -f "%E" ./target/$TARGET/rambo test-data/$i $@ > /dev/null | perl -pe 's/.\w+: (.*)/\1/g'
+	/usr/bin/time -f "%E" ./target/$TARGET/rambo "$SRC_DIR/$i" $@ > /dev/null | perl -pe 's/.\w+: (.*)/\1/g'
 done
